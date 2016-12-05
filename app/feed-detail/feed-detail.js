eae18f5b1c41b78a44b5633a2a2df807e3210cfa
Aderick Krispradhana <aderick.k@gmail.com>
2016-12-05 16:40:22 +0100

    test

--- app/feed-detail/feed-detail.js
+++ app/feed-detail/feed-detail.js
@@ -4,16 +4,16 @@ angular
 	.module('feedDetail', ['ngRoute'])
 	.component('feedDetail', {
 		templateUrl: 'feed-detail/feed-detail.html',
-		controller: 
+		controller:
 			function FeedDetailController($routeParams, $http) {
 				var self = this;
 				var reqId = $routeParams.feedId;
 				var reqLink = 'jsonlist/element' + reqId + '.json';
-				
+
 				$http.get(reqLink).then(
 					function (response){
 						self.element = response.data;
 					}
 				);
-			}		
+			}
 	});
